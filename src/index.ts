0f98289e974d452ca998ecadd53b1a587150033b
t-tomek <tok.tomasz@gmail.com>
2020-02-02 20:46:25 +0100

    export crs & projections

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import Bounds from "./geometry/bounds";
 import Coordinates from "./geo/coordinates";
 import CoordinatesBounds from "./geo/coordinatesBounds";
+import Epsg3857 from "./geo/crs/crsEpsg3857";
 import Map from "./map"; 
 import Marker from "./layer/marker/marker";
+import MercatorProjection from "./geo/projection/mercator";
 import Point from "./geometry/point";
+import SphericalMercatorProjection from "./geo/projection/sphericalMercator";
 import TileLayer from "./layer/tile/tileLayer";
 
 export {
     Bounds,
     Coordinates,
     CoordinatesBounds,
+    Epsg3857,
     Map,
     Marker,
+    MercatorProjection,
     Point,
+    SphericalMercatorProjection,
     TileLayer,
 };
